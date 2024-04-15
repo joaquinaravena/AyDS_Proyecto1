ab43a4681ec9768005c6b1578f961bb9e9635e46
Joaquin <joaco.aravena02@gmail.com>
2024-04-15 18:54:59 -0300

    Cambios refactor

--- app/src/main/java/ayds/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -1,135 +1,124 @@
-package ayds.songinfo.moredetails.fulllogic;
+package ayds.songinfo.moredetails.fulllogic
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.TextView;
-import androidx.room.Room;
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.text.Html
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.room.Room.databaseBuilder
+import ayds.songinfo.R
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.squareup.picasso.Picasso
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.io.IOException
+import java.util.*
 
-import ayds.songinfo.R;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.squareup.picasso.Picasso;
+class OtherInfoWindow : Activity() {
+    private var textPane1: TextView? = null
+    private var dataBase: ArticleDatabase? = null
 
-import java.io.IOException;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.activity_other_info)
 
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+        textPane1 = findViewById(R.id.textPane1)
 
+        open(intent.getStringExtra(ARTIST_NAME_EXTRA))
+    }
 
+    private fun getArtistInfo(artistName: String?) {
+        val lastFMAPI = getLastFMAPI()
 
-public class OtherInfoWindow extends Activity {
+        Thread {
+            val article = dataBase!!.ArticleDao().getArticleByArtistName(artistName!!)
+            var text = ""
 
-  public final static String ARTIST_NAME_EXTRA = "artistName";
-  public static final String BASE_URL = "https://ws.audioscrobbler.com/2.0/";
-  public static final String IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png";
-  private TextView textPane1;
-  private ArticleDatabase dataBase = null;
+            if (article != null) {
+                text = "[*]" + article.biography
+                addListenerToOpenUrlButton1(article.articleUrl)
+            } else {
+                val callResponse: Response<String>
+                try {
+                    callResponse = lastFMAPI.getArtistInfo(artistName).execute()
 
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+                    val gson = Gson()
+                    val jobj = gson.fromJson(callResponse.body(), JsonObject::class.java)
+                    val artist = jobj["artist"].asJsonObject
+                    val bio = artist["bio"].asJsonObject
+                    val extract = bio["content"]
+                    val url = artist["url"]
 
-    setContentView(R.layout.activity_other_info);
+                    when (extract) {
+                        null -> {
+                            text = "No Results"
+                        }
+                        else -> {
+                            text = extract.asString.replace("\\n", "\n")
+                            text = textToHtml(text, artistName)
+                            Thread { dataBase!!.ArticleDao().insertArticle(ArticleEntity(artistName, text, url.asString)) }
+                                .start()
+                        }
+                    }
+                    addListenerToOpenUrlButton1(url.asString)
+                } catch (e1: IOException) {
+                    e1.printStackTrace()
+                }
+            }
+            loadText(text)
+        }.start()
+    }
 
-    textPane1 = findViewById(R.id.textPane1);
+    private fun loadText(text: String) {
+        runOnUiThread {
+            Picasso.get().load(IMAGE_URL).into(findViewById<View>(R.id.imageView1) as ImageView)
+            textPane1!!.text = Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY)
+        }
+    }
 
-    open(getIntent().getStringExtra(ARTIST_NAME_EXTRA));
-  }
-
-  public void getArtistInfo(String artistName) {
-
-    Retrofit retrofit = new Retrofit.Builder()
+    private fun getLastFMAPI(): LastFMAPI {
+        val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(ScalarsConverterFactory.create())
-            .build();
+            .build()
 
-    LastFMAPI lastFMAPI = retrofit.create(LastFMAPI.class);
+        val lastFMAPI = retrofit.create(LastFMAPI::class.java)
+        return lastFMAPI
+    }
 
-    Log.e("TAG","artistName " + artistName);
+    private fun addListenerToOpenUrlButton1(urlArticle: String) {
+        findViewById<View>(R.id.openUrlButton1).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(urlArticle))
+            startActivity(intent)
+        }
+    }
 
-        new Thread(() -> {
+    private fun open(artist: String?) {
+        dataBase = databaseBuilder(this, ArticleDatabase::class.java, "database-name-thename").build()
+        getArtistInfo(artist)
+    }
 
-          ArticleEntity article = dataBase.ArticleDao().getArticleByArtistName(artistName);
+    companion object {
+        const val ARTIST_NAME_EXTRA: String = "artistName"
+        const val BASE_URL: String = "https://ws.audioscrobbler.com/2.0/"
+        const val IMAGE_URL: String =
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
 
-          String text = "";
+        fun textToHtml(text: String, term: String?): String {
+            val textWithBold = text
+                .replace("'", " ")
+                .replace("\n", "<br>")
+                .replace("(?i)$term".toRegex(), "<b>" + term!!.uppercase(Locale.getDefault()) + "</b>")
 
-          if (article != null) {
-            text = "[*]" + article.getBiography();
-            addListenerToOpenUrlButton1(article.getArticleUrl());
-
-          } else {
-            Response<String> callResponse;
-            try {
-              callResponse = lastFMAPI.getArtistInfo(artistName).execute();
-
-              Log.e("TAG","JSON " + callResponse.body());
-
-              Gson gson = new Gson();
-              JsonObject jobj = gson.fromJson(callResponse.body(), JsonObject.class);
-              JsonObject artist = jobj.get("artist").getAsJsonObject();
-              JsonObject bio = artist.get("bio").getAsJsonObject();
-              JsonElement extract = bio.get("content");
-              JsonElement url = artist.get("url");
-
-
-              if (extract == null) {
-                text = "No Results";
-              } else {
-                text = extract.getAsString().replace("\\n", "\n");
-
-                text = textToHtml(text, artistName);
-
-                final String text2 = text;
-                new Thread(() ->
-                        dataBase.ArticleDao().insertArticle(new ArticleEntity(artistName, text2, url.getAsString()))).start();
-              }
-              addListenerToOpenUrlButton1(url.getAsString());
-
-            } catch (IOException e1) {
-              Log.e("TAG", "Error " + e1);
-            }
-          }
-
-          Log.e("TAG","Get Image from " + IMAGE_URL);
-
-          String finalText = text;
-          runOnUiThread( () -> {
-            Picasso.get().load(IMAGE_URL).into((ImageView) findViewById(R.id.imageView1));
-
-            textPane1.setText(Html.fromHtml(finalText, Html.FROM_HTML_MODE_LEGACY));
-          });
-        }).start();
-  }
-
-  private void addListenerToOpenUrlButton1(String urlArticle) {
-    findViewById(R.id.openUrlButton1).setOnClickListener(viewOnClick -> {
-      Intent intent = new Intent(Intent.ACTION_VIEW);
-      intent.setData(Uri.parse(urlArticle));
-      startActivity(intent);
-    });
-  }
-
-  private void open(String artist) {
-    dataBase = Room.databaseBuilder(this, ArticleDatabase.class, "database-name-thename").build();
-    getArtistInfo(artist);
-  }
-
-  public static String textToHtml(String text, String term) {
-
-    String textWithBold = text
-            .replace("'", " ")
-            .replace("\n", "<br>")
-            .replaceAll("(?i)" + term, "<b>" + term.toUpperCase() + "</b>");
-
-    return "<html><div width=400>" + "<font face=\"arial\">" +
-            textWithBold + "</font></div></html>";
-  }
-
+            return "<html><div width=400>" + "<font face=\"arial\">" +
+                    textWithBold + "</font></div></html>"
+        }
+    }
 }
